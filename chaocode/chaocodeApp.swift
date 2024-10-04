f281a476d5799e94cc6c59a713a463ec942c40eb
lingyoudao <lingyoudao@hotmail.com>
2024-10-04 14:09:30 +0800

    Initial Commit

---
+++ chaocode/chaocodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  chaocodeApp.swift
+//  chaocode
+//
+//  Created by 凌有道 on 2024/10/4.
+//
+
+import SwiftUI
+
+@main
+struct chaocodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
